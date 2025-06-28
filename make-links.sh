2b5906948817e3280df56902a86fadb86e9c8935
Peter A. Geer <pageer@Altron>
2025-06-28 13:28:18 -0400

    Add nvim config to script

--- make-links.sh
+++ make-links.sh
@@ -4,3 +4,4 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ln -s "$script_dir/_vimrc" $HOME/.vimrc
 ln -s "$script_dir/vimfiles" $HOME/.vim
+ln -s "$script_dir/vimfiles/nvim-config.lua" $HOME/.config/nvim/init.lua
